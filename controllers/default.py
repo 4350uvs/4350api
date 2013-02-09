5d7be5729fbd7be23ec23e5c46faca393545287d
Zhao Han <cx.chenghai+github@gmail.com>
2013-02-09 21:14:34 +0000

    def api() in default controller as RESTful service, only GET polls/ for now

--- controllers/default.py
+++ controllers/default.py
@@ -1,2 +1,10 @@
 def index():
-  return "Hello 4350api: no view, only JSON";
+    return "Hello 4350api: no view, only JSON";
+
+@request.restful()
+def api():
+    def GET(first_arg):
+        if not first_arg == 'polls': raise HTTP(400)
+        polls = db().select(db.polls.ALL).as_list()
+        return dict(polls = polls)
+    return locals();
