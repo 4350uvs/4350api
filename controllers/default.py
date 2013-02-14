e1f0a7a356f6db88dc4783d6ce17385b3d50ee41
Zhao Han <cx.chenghai+github@gmail.com>
2013-02-14 03:23:29 +0000

    def POST() inside api(): implement POST /polls

--- controllers/default.py
+++ controllers/default.py
@@ -3,8 +3,24 @@ def index():
 
 @request.restful()
 def api():
+
     def GET(first_arg):
         if not first_arg == 'polls': raise HTTP(400)
         polls = db().select(db.polls.ALL).as_list()
         return dict(polls = polls)
+
+    def POST(first_arg, **params):
+        if not first_arg == 'polls': raise HTTP(400)
+        if not check_params(['title', 'choice'], params): raise HTTP(400)
+
+        # insert poll
+        pid = db.polls.insert(title = params['title'])
+        # insert poll choices
+        choice_param = params['choice']
+        if isinstance(choice_param, str): choice_param = [choice_param]
+        for choice in choice_param:
+            db.pollChoices.insert(pid = pid, content = choice)
+
+        raise HTTP(201, pid)
+
     return locals();
