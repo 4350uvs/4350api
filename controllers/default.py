b43d6992b47c04ae93daffc4629c7882e1c75e97
Zhao Han <cx.chenghai+github@gmail.com>
2013-02-27 05:28:54 +0000

    Support /poll/:pid: only id and title for now
     - move "raise HTTP(400 xxx" to api helper
     - format POST method

--- controllers/default.py
+++ controllers/default.py
@@ -4,20 +4,40 @@ def index():
 @request.restful()
 def api():
 
-    def GET(first_arg):
-        if not first_arg == 'polls': raise HTTP(400, **{'Access-Control-Allow-Origin': '*'})
-        polls = db().select(db.polls.ALL).as_list()
-        return dict(polls = polls)
+    def GET(*args, **params):
+        if len(args) == 0 or args[0] != 'polls':
+            raise bad_request()
+
+        if len(args) == 1:
+            # all polls
+            polls = db().select(db.polls.ALL).as_list()
+            return dict(polls = polls)
+        elif len(args) == 2 and args[1].isdigit():
+            # specific poll
+            pid = args[1]
+            poll = db.polls[pid]
+
+            if poll is not None:
+                # TODO query all poll options
+                return dict(poll = poll)
+            else:
+                raise not_found();
+        else:
+            return locals();
 
     def POST(first_arg, **params):
-        if not first_arg == 'polls': raise HTTP(400, **{'Access-Control-Allow-Origin': '*'})
-        if not check_params(['title', 'choice'], params): raise HTTP(400, **{'Access-Control-Allow-Origin': '*'})
+        if not first_arg == 'polls':
+            raise bad_request()
+
+        if not check_params(['title', 'choice'], params):
+            raise bad_request()
 
         # insert poll
         pid = db.polls.insert(title = params['title'])
         # insert poll choices
         choice_param = params['choice']
-        if isinstance(choice_param, str): choice_param = [choice_param]
+        if isinstance(choice_param, str):
+            choice_param = [choice_param]
         for choice in choice_param:
             db.pollChoices.insert(pid = pid, content = choice)
 
